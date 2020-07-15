e78ae736fcf6415ce395f5649299c468fb007ae3
Alejandro Manuel Fidanza <tushita@macbooktushita.local>
2020-07-14 20:27:45 -0500

    updates

--- pages/contact.js
+++ pages/contact.js
@@ -89,7 +89,7 @@ class Contact extends Component {
     
     if (!submitted && !submitting) {
       this.setState({submitting:true})
-      fetch('https://clarividenciafotografia.com/contact', {
+      fetch('/contact', {
         method: 'post',
         headers: {
           'Accept': 'application/json, text/plain, */*',
